e8eec36e46802ac57d761c4275ad08bb85458c82
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-11 20:07:48 +0600

    added images, theme

---
+++ src/layouts/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <nav>this is nav</nav>
+            <Outle
+        </div>
+    );
+};
+
+export default Main;
